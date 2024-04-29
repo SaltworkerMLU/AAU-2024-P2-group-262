27b949a0046187c365383237ce67bf3f69e99a4e
Mathias Lykholt-Ustrup <saltworkermlu@gmail.com>
2024-04-29 08:07:56 +0200

    Add files via upload

--- UR5.m
+++ UR5.m
@@ -9,7 +9,7 @@ classdef UR5
         % Predefined transform matrices of the UR5
         TB0 = [eye(4,3) [0;0;89.2;1]]; % Transform matrix from base to joint 0 % d(1) = 89.2
         T6W = diag([-1 -1 1 1]); % Diagonal 4x4-matrix using eigenvalues [-1, -1, 1, 1]
-        a_max = 1; %2922
+        a_max = 0.9; %2922
         v_max = 10;
     end
 
@@ -32,8 +32,8 @@ classdef UR5
             T06 = UR5.TB0 \ TBW / UR5.T6W; % T06 is calculated using the equation: T06 = TB0^(-1) * TBW * T6W^(-1)
         end
 
-        function Joint = inverseKinematics(T06)
-            % Solutions are provided as "Joint values" = Joint(solution, joint)
+        function solutions = inverseKinematics(T06)
+            % Solutions are provided as 8 sets of "Joint values"
             Joint = zeros(8,6);
             for i = 1:8 % For all 8 solutions
                 %theta1: Has 2 possible solutions
@@ -46,9 +46,9 @@ classdef UR5
                 %theta5: Has 2 possible solutions (depending on theta1)
                 T01 = UR5.TB0 \ UR5.forwardKinematics(Joint(i,:), 1, 1); % T01 = TB0^(-1) * TB1, essentially d(1) is disregarded, as it is nullified using TB0
                 T16 = T01 \ T06 ; % T16 = T01^(-1) * T06
-                phi = acos((-T16(2,4)-UR5.d(4))/UR5.d(6));
-                phi1 = [phi -phi]; % a 1x2 vector containing each possible solution of theta5...
-                Joint(i,5) = phi1(1+mod(round((i-1)/4),2)); % theta5 % ... solution is chosen using equation 1+mod(round((i-1)/4),2)
+                phi1 = acos((-T16(2,4)-UR5.d(4))/UR5.d(6));
+                phi = [phi1 -phi1]; % a 1x2 vector containing each possible solution of theta5...
+                Joint(i,5) = phi(1+mod(round((i-1)/4),2)); % theta5 % ... solution is chosen using equation 1+mod(round((i-1)/4),2)
 
                 %theta6: Has 4 possible solution (depending on theta1)
                 T60 = inv ( T06 ) ;
@@ -99,108 +99,166 @@ classdef UR5
                 if isnan(Joint(i))
                     Joint(i,:) = [];
                 end
-            end      
-
+            end
+            
+            solutions = zeros(size(Joint,1)*64, 6); % 512 possible solutions...
+            
+            for i = 1:64 % ... 8 of which are unique (512/8 = 64)
+                temp = Joint;
+                % Configuration of which joints to change by 360 degrees
+                % using i in a binary context, e.g. 001000 = 4 & 000000 = 0
+                config = [1-mod(i,2) 1-mod(round(i/2),2) mod(round((i-1)/8),2) mod(round((i-1)/16),2) mod(round((i-1)/32),2) mod(round((i-1)/64),2)];
+                for j = 1:size(Joint,1) % For all 8 unique solutions
+                    for k = 1:6 % For all 6 joints
+                        if config(k) == 0 % If config has not changed... %lastConfig(k)
+                            continue % ... pass
+                        end
+                        if temp(j,k) + 360 > 360
+                            temp(j,k) = temp(j,k) - 360;
+                        else
+                            temp(j,k) = temp(j,k) + 360;
+                        end
+                    end
+                    solutions((i-1)*size(Joint,1)+ j,:) = temp(j,:); % Set i'th set of joint solutions
+                end
+            end
         end
         
-        function moveJ(P0, Pf)
-            RDK = Robolink; % Generate a Robolink object RDK. This object interfaces with RoboDK.
-            robot = RDK.ItemUserPick('Select one robot', RDK.ITEM_TYPE_ROBOT); % Select robot
-            if robot.Valid() == 0
-            error('No robot selected'); % Missing robot
-            end
-            ref = robot.Parent();
-
+        function tf = moveJtf(P0, Pf)
             P0 = 180/pi * P0; % Convert to degrees
             Pf = 180/pi * Pf; % Convert to degrees
-            P0dot = [0 0 0 0 0 0]; % Assuming no start velocity
-            Pfdot = [0 0 0 0 0 0]; % Assuming no end velocity
 
-            tf_a = max(abs(sqrt(4*(Pf - P0)/UR5.a_max))); 
+            tf_a = max(abs(sqrt(4*(Pf - P0)/UR5.a_max)));
+            tf_v = max(abs((Pf - P0)/UR5.v_max)); % t = ds/v
             tb = UR5.v_max / UR5.a_max; % t = v / a
-            thetab = P0 + 1/2 * UR5.a_max * tb.^2;
-            thetabneg = Pf - 1/2 * UR5.a_max * tb.^2; % thetab = Pf - 1/2 * UR5.a_max * (tf - t)^2 % t
-            tf_v = max(abs((thetabneg - thetab)/UR5.v_max)); % t = ds/v
 
             if 2*tb > tf_a % If acceleration reaches max.
                 tf = tf_a;
             else % if velocity hits max
-                tf = tf_v;
+                tf = tf_v + tb;
             end
-            disp("Execution time of moveJ(): " + tf);
-
-            %t=0:0.002:tf; % Control frequency: 500 hz
-            t=0:0.05:tf; % Control frequency: 20 hz
-            
-            % Cubic polynomal parameters
-            a0 = P0;
-            a1 = P0dot;
-            a2 = 3/(tf.^2)*(Pf-P0)-2/tf*P0dot-1/tf*Pfdot;
-            a3 = -2/(tf.^3)*(Pf-P0)+1/(tf.^2)*(Pfdot+P0dot);
-            
-            %progJoint = RDK.AddProgram('MoveJ Cubic');
-            %progJoint.addMoveJ(RDK.Item('start')); %MoveJ (start)
-            
-            % Calculate frames in 6x1 vector [x y z gamma beta alpha]
-            for i=2:(length(t)-1)
-                P=a0+a1*t(i)+a2*t(i)^2+a3*t(i)^3; % Aquire position
-                robot.setJoints(P); % via. point in RoboDK
-                %targetname = sprintf('TargetJ%i',i);
-                %target = RDK.AddTarget(targetname,ref,robot);
-                %progJoint.addMoveJ(target);
-            end
-            %progJoint.addMoveJ(RDK.Item('end')); %MoveJ (end)
+            disp("Execution time of moveJ(): " + tf + "s");
         end
 
-        function moveL(P0, Pf, robot)
+        function tf = moveLtf(P0, Pf)
             TBS = UR5.forwardKinematics(P0, 1, 6); % Use only TBW of P0
             TBE = UR5.forwardKinematics(Pf, 1, 6); % Use only TBW of Pf
-
-            startLoc = Pose_2_Fanuc(TBS)'; % Acquire XYZRPW of TBS % Fanuc/KUKA/XYZRPY don't matter
-            endLoc = Pose_2_Fanuc(TBE)'; % Acquire XYZRPW of TBE
-
-            disp(startLoc)
-
-            tf_a = max(abs(sqrt(4*(endLoc - startLoc)/UR5.a_max))); 
-            tb = UR5.v_max / UR5.a_max; % t = v / a
-            thetab = startLoc + 1/2 * UR5.a_max * tb.^2;
-            thetabneg = endLoc - 1/2 * UR5.a_max * tb.^2; % thetab = Pf - 1/2 * UR5.a_max * (tf - t)^2 % t
-            tf_v = max(abs((thetabneg - thetab)/UR5.v_max)); % t = ds/v
-
-            if 2*tb > tf_a % If acceleration reaches max.
-                tf = tf_a;
-            else % if velocity hits max
-                tf = tf_v;
-            end
-
-            t=0:0.05:tf; % 20fps  
-            disp("Execution time of moveL(): " + tf);
 
             Tse = TBS \ TBE; %Compute Tse (Angle-axis representation)
             theta = acos((Tse(1,1)+Tse(2,2)+Tse(3,3)-1)/2); %Find axis angle
             Ps = [0 0 0 0]; % Start frame position (origo)
             Pe = [Tse(1,4) Tse(2,4) Tse(3,4) theta]; % end postion relative to start frame
-            Psdot = [0 0 0 0]; % Assuming no via points from "start" to "end"
-            Pedot = [0 0 0 0]; % Assuming no via points from "start" to "end"
-            k=1/(2*sin(theta)) * [Tse(3,2)-Tse(2,3);Tse(1,3)-Tse(3,1);Tse(2,1)-Tse(1,2)];
 
-            % Cubic polynomal parameters
-            a0 = Ps;
-            a1 = Psdot;
-            a2 = 3/(tf.^2)*(Pe-Ps)-2/tf*Psdot-1/tf*Pedot;
-            a3 = -2/(tf.^3)*(Pe-Ps)+1/(tf.^2)*(Pedot+Psdot);
+            tf_a = sqrt(4*norm(Pe - Ps)/UR5.a_max); 
+            tf_v = norm(Pe - Ps)/UR5.v_max;
+            tb = UR5.v_max / UR5.a_max; % t = v / a
 
-            % Calculate frames in 4x1 vector [x y z theta]
-            for i=2:(length(t)-1)
-                P=a0+a1*t(i)+a2*t(i)^2+a3*t(i)^3; % Aquire position
-                theta=P(4); % aquire theta from P
-                TSI= [k(1)*k(1)*(1-cos(theta))+cos(theta)     k(1)*k(2)*(1-cos(theta))-k(3)*sin(theta) k(1)*k(3)*(1-cos(theta))+k(2)*sin(theta) P(1);
-                     k(1)*k(2)*(1-cos(theta))+k(3)*sin(theta)  k(2)*k(2)*(1-cos(theta))+cos(theta)    k(2)*k(3)*(1-cos(theta))-k(1)*sin(theta) P(2) ;
-                    k(1)*k(3)*(1-cos(theta))-k(2)*sin(theta)  k(3)*k(2)*(1-cos(theta))+k(1)*sin(theta) k(3)*k(3)*(1-cos(theta))+cos(theta) P(3);
-                    0 0 0 1;]; % Convert [x y z theta] to 4x4 transform matrix
-                TBI=TBS*TSI; 
-                robot.setPose(TBI); % via. point in RoboDK
+            if 2*tb > tf_a % If acceleration reaches max.
+                tf = tf_a;
+            else % if velocity hits max
+                tf = tf_v + tb;
             end
+            disp("Execution time of moveL(): " + tf + "s");
+        end
+
+        function stateDot = exampleHelperTimeBasedJointInputs(motionModel, timeInterval, configWaypoints, t, state)
+            % This function is for internal use only and may be removed in a future
+            % release
+
+            %exampleHelperTimeBasedJointInputs Pass time-varying inputs to the jointSpaceMotionModel derivative
+            %   Since the jointSpaceMotionModel derivative method is updated at an
+            %   instant in time, a wrapper function is needed to provide time-varying
+            %   tracking inputs. This function computes the value of the B-spline
+            %   trajectory at an instant in time, t, and provides that input to the
+            %   derivative of the associated jointSpaceMotionModel at that same
+            %   instant. The resultant state derivative can be passed to an ODE solver
+            %   to compute the tracking behavior.
+
+            % Copyright 2019 The MathWorks, Inc.
+
+            % Use a B-spline curve to ensure the trajectory is smooth and moves
+            % through the waypoints with non-zero velocity
+            [qd, qdDot] = bsplinepolytraj(configWaypoints,  timeInterval , t);
+    
+            % Compute state derivative
+            stateDot = derivative(motionModel, state, [qd; qdDot]);
+        end
+
+        % https://se.mathworks.com/help/robotics/ug/plan-and-execute-trajectory-kinova-gen3.html
+        function moveJ(P0, Pf)
+            ur5_RBT = loadrobot('universalUR5','DataFormat','row');
+            endEffector = "tool0"; % In case another robot is used, analyse ur5_RBT properties/variables
+
+            % Joint space-trajectory
+            tInterval = 0:UR5.moveJtf(P0, Pf); %Time interval displayed using the method % 0:timeStep=1:UR5.moveLtf(P0, Pf)
+            jointConfigArray = cubicpolytraj([P0',Pf'],[tInterval(1); tInterval(end)],tInterval);
+            jsMotionModel = jointSpaceMotionModel('RigidBodyTree',ur5_RBT,'MotionType','PDControl');
+            [tJoint,stateJoint] = ode15s(@(t,state) UR5.exampleHelperTimeBasedJointInputs(jsMotionModel,[tInterval(1); tInterval(end)],jointConfigArray,t,state), [tInterval(1); tInterval(end)],[P0,zeros(size(P0))]);
+
+            % Plotting
+            figure;
+            show(ur5_RBT,P0,'PreservePlot',false,'Frames','off');
+            hold on
+            axis([-1 1 -1 1 -0.1 1.5]);
+            for i=1:length(tInterval)
+                % Interpolate simulated joint positions to get configuration at current time
+                configNow = interp1(tJoint,stateJoint(:,1:6),tInterval(i)); % Interpolate using time, state, joint [1,6], current time
+                poseNow = getTransform(ur5_RBT,configNow,endEffector);
+                show(ur5_RBT,configNow,'PreservePlot',false,'Frames','off');
+                jointSpaceMarker = plot3(poseNow(1,4),poseNow(2,4),poseNow(3,4),'r.','MarkerSize',20);
+                drawnow; % Draw trajectory iteratively
+            end
+            %legend(jointSpaceMarker, {'Defined in Joint-Space'}); % Add a legend
+            title('MoveJ()') % Add a title
+        end
+
+        function stateDot = exampleHelperTimeBasedTaskInputs(motionModel, timeInterval, initialTform, finalTform, t, state)
+            % This function is for internal use only and may be removed in a future
+            % release
+
+            %exampleHelperTimeBasedTaskInputs Pass time-varying inputs to the taskSpaceMotionModel derivative
+            %   Since the taskSpaceMotionModel derivative method is updated at an
+            %   instant in time, a wrapper function is needed to provide time-varying
+            %   tracking inputs. This function computes the value of the transform
+            %   trajectory at an instant in time, t, and provides that input to the
+            %   derivative of the associated taskSpaceMotionModel at that same instant.
+            %   The resultant state derivative can be passed to an ODE solver to
+            %   compute the tracking behavior.
+
+            % Copyright 2019 The MathWorks, Inc.
+
+            [refPose, refVel] = transformtraj(initialTform, finalTform, timeInterval, t);
+
+            stateDot = derivative(motionModel, state, refPose, refVel);
+        end
+        
+        % https://se.mathworks.com/help/robotics/ug/plan-and-execute-trajectory-kinova-gen3.html
+        function moveL(P0, Pf)
+            ur5_RBT = loadrobot('universalUR5','DataFormat','row');
+            endEffector = "tool0"; % In case another robot is used, analyse ur5_RBT properties/variables
+            
+            % Task space-trajectory
+            taskInit = getTransform(ur5_RBT,P0,endEffector);
+            taskFinal = getTransform(ur5_RBT,Pf,endEffector);
+            tInterval = 0:UR5.moveLtf(P0, Pf); %Time interval displayed using the method % 0:timeStep=1:UR5.moveLtf(P0, Pf)
+            tsMotionModel = taskSpaceMotionModel('RigidBodyTree',ur5_RBT,'EndEffectorName',endEffector); % Cartesian space-trajectory motion
+            [tTask,stateTask] = ode15s(@(t,state) UR5.exampleHelperTimeBasedTaskInputs(tsMotionModel,[tInterval(1); tInterval(end)],taskInit,taskFinal,t,state),[tInterval(1); tInterval(end)],[P0, zeros(size(P0))]);
+            
+            % Plotting
+            figure;
+            show(ur5_RBT,P0,'PreservePlot',false,'Frames','off'); % Extra params to update UR5 position
+            hold on
+            axis([-1 1 -1 1 -0.1 1.5])
+            for i=1:length(tInterval)
+                % Interpolate simulated joint positions to get configuration at current time
+                configNow = interp1(tTask,stateTask(:,1:6),tInterval(i)); % Interpolate using time, state, joint [1,6], current time
+                poseNow = getTransform(ur5_RBT,configNow,endEffector);
+                show(ur5_RBT,configNow,'PreservePlot',false,'Frames','off');
+                taskSpaceMarker = plot3(poseNow(1,4),poseNow(2,4),poseNow(3,4),'b.','MarkerSize',20);
+                drawnow; % Draw trajectory iteratively
+            end
+            %legend(taskSpaceMarker, {'Defined in Task-Space'}); % Add a legend
+            title('MoveL()') % Add a title
         end
 
         function J = jacobian(joint)
--- UR5example.m
+++ UR5example.m
@@ -13,34 +13,94 @@ UR5; % Create object
 
 Ps = pi/180 * RDK.Item('start').Joints();
 Pe = pi/180 * RDK.Item('end').Joints();
-% P4 = RDK.Item('Target 3').Joints();
 %P0 = pi/180 * [0 ,-90, -90, 0, 90, 0];
-%Pf = pi/180 * [-59.24 ,-124.95, -78.80, 25.82, 89.62, 107.79];
-%P2 = pi/180 * [0 ,0, 0, 0, 90, 0];
 
+[TBW, T06] = UR5.forwardKinematics(Pe, 1, 6); % Use forward kinematics to acquire transform matrices
 
-%[TBW, T06] = UR5.forwardKinematics(P0, 1, 6); % Use forward kinematics to acquire transform matrices
-[TBW, T06] = UR5.forwardKinematics(Ps, 1, 6); % Use forward kinematics to acquire transform matrices
+disp(TBW)
 
+%solution = UR5.inverseKinematics(T06);
 solution = UR5.inverseKinematics(T06);
 
-IK = robot.SolveIK_All(TBW); % Provides all solution in JOINTxSOLUTION matrix
-IK(8,:) = []; % Remove 8th "joint" (hint: It shouldn't exist)
-IK(7,:) = []; % Remove 7th "joint" (hint: It shouldn't exist)
-disp("!Position has: " + length(IK) + " solutions")
-disp(IK.'); % Transpose to get SOLUTIONxJOINT matrix
 
-disp("MatLab solution:")
-disp(solution)
+%IK = robot.SolveIK_All(TBW); % Provides all solution in JOINTxSOLUTION matrix
+%IK(8,:) = []; % Remove 8th "joint" (hint: It shouldn't exist)
+%IK(7,:) = []; % Remove 7th "joint" (hint: It shouldn't exist)
+%disp("!Position has: " + length(IK) + " solutions")
+%disp(IK.'); % Transpose to get SOLUTIONxJOINT matrix
 
-progJoint = RDK.AddProgram('IK config');
+%disp(length(solution))
 
-for i = 1:length(solution)
+%disp("MatLab solution:")
+%disp(solution)
+
+%shortest = solution(1,:);
+farthest = solution(1,:);
+
+%timer = zeros(size(solution,1),1);
+
+%progJoint = RDK.AddProgram('Start->end');
+
+% Use 8 IK solution where joints are within range [-180, 180]
+% Add said moveJ() commands to RoboDk
+for i = 1:8 %size(solution,1)
+    programname = sprintf('Start->end%i',i);
+    progJoint = RDK.AddProgram(programname);
+
+    timer(i) = UR5.moveJtf(Ps, solution(i,:)*pi/180);
     robot.setJoints(solution(i,:)); % via. point in RoboDK
-    targetname = sprintf('TargetL%i',i);
-    target = RDK.AddTarget(targetname,ref,robot);
-    progJoint.addMoveJ(target);
+
+    targetname = sprintf('Target%i',i);
+    target = RDK.AddTarget(targetname, ref, robot);
+    target.setJoints(solution(i,:));
+
+    progJoint.addMoveJtf(target);
 end
 
-UR5.moveL(Ps, Pe, robot);
+% Add moveL() to RoboDK
+programname = sprintf('moveL_Start->end');
+progJoint = RDK.AddProgram(programname);
+progJoint.addMoveL(target);
+
+
+maxim = find(timer == max(timer));
+minim = find(timer == min(timer));
+%disp(Ps*180/pi)
+
+disp(max(timer))
+disp(solution(maxim(1),:))
+disp(min(timer))
+disp(solution(minim,:))
+
 UR5.moveJ(Ps, Pe);
+UR5.moveL(Ps, Pe);
+
+%UR5.moveJ(Ps,solution(minim(1),:)*pi/180)
+
+%UR5.moveJ(Ps, solution(minim(1,:),:)*pi/180);
+%disp(find(min(timer)))
+%disp(solution(),:))
+
+%disp("Shortest path:")
+%disp(shortest)
+
+%disp("Farthest path:")
+%disp(farthest)
+
+%progJoint = RDK.AddProgram('IK config');
+
+%for i = 1:length(solution)
+%    robot.setJoints(solution(i,:)); % via. point in RoboDK
+    %targetname = sprintf('TargetL%i',i);
+    %target = RDK.AddTarget(targetname,ref,robot);
+    %progJoint.addMoveJ(target);
+%end
+
+%disp(TBW)
+%disp(Pose_2_XYZRPW(TBW))
+
+%UR5.moveL(Ps, Pe, robot);
+%UR5.moveJ(Ps, Pe);
+
+%UR5.moveJ(Ps, farthest*pi/180);
+%UR5.moveJ(Ps, shortest*pi/180);
